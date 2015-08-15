c0e71f4ce46168d94a78877223214739ada31095
ShaoJun <music1913@gmail.com>
2015-08-15 22:24:43 +0800

    commit from pycharm

--- services/urls.py
+++ services/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^building/$', views.Building_list),
     url(r'^building/(?P<pk>[0-9]+)/$', views.Building_detail),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
